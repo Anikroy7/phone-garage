a986f73cdd4a35eb1ad2b913d9ec1d37c7da7408
Anikroy7 <roy645800@gmail.com>
2022-03-01 19:20:15 +0600

    load details with button

--- phone.js
+++ phone.js
@@ -13,11 +13,12 @@ const loadPhone = () => {
 }
 
 const displayPhone = phones => {
-    console.log(phones)
+    // console.log(phones)
     const displayId = document.getElementById('display-phones');
+    displayId.textContent = '';
     console.log(displayId)
     for (const phone of phones) {
-        console.log(phone)
+        // console.log(phone)
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
@@ -28,11 +29,20 @@ const displayPhone = phones => {
                     <h5 class="card-title">Phone Name: ${phone.phone_name}<h5>
                     <p class="card-text">Brand Name: ${phone.brand}</p>
                 </div>
-                
+                <button type="button" onclick="loadShowDetails('${phone.slug}')" class="btn btn-secondary fst-normal border rounded">Show details</button>
            </div>
         
         `
         displayId.appendChild(div);
     }
 
+}
+
+const loadShowDetails = (phoneId) => {
+    // console.log(phoneId)
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+    console.log(url)
 }
